ffc0ae01a9c8bf8020ecf0f91a36ce3f2d838b9e
1804449@kiit.ac.in <1804449@kiit.ac.in>
2020-08-16 20:47:08 +0530

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import {BrowserRouter} from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
-
-
+const store = ConfigureStore();
 class App extends Component {
     
     render() {
         return (
+            <Provider store={store}>
             <BrowserRouter>
             <div className='App'>
                 <Main/>
             </div>
             </BrowserRouter>
+            </Provider>
         );
     }
 }
